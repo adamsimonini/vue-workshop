68da9e6fb1921a45b949f738fad06c0972a2f05d
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-08-13 11:17:15 -0400

    MagesticPotatoe's suggested vue instance change

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
+const opts = {
+  icons: {
+    iconfont: 'mdi',
+  },
+};
+Vue.use(Vuetify);
+
 new Vue({
-  router,
   store,
-  vuetify,
+  router,
+  vuetify: new Vuetify(opts),
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
