2740e6fb2d41a8b9225a9b995137049c4a2d674b
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-09-23 12:50:05 -0400

    Updates to pathing text style

--- src/views/Routing.vue
+++ src/views/Routing.vue
@@ -3,7 +3,7 @@
       <h2>Routing</h2>
       <p>
           Today's lesson is just about over, but there is one more item of interest. Have you noticed that we could flip through content from the agenda without reloading the page?
-          That works due to Vue's router. Routing doesn't come natively in Vue.js, but is a common and very popular addon. Checkout "src/router.js". 
+          That works due to Vue's router. Routing doesn't come natively in Vue.js, but is a common and very popular addon. Checkout <span class="path">"src/router.js"</span>. 
           There, I have imported most of the page components (the components located in the "views" folder). Then, in the 'route' array, a given path is associated with 
           a given imported component. When the url matches that route, the router flips to that component. 
           Then, in the "src/App.vue" file, you can find a router-view component that simply shows whatever the router says to. (No further coding required.)
