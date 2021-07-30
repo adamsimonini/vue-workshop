b92ca0bbbe1c432ddc1bf7e682865a7dc4bbb289
Adam Simonini <adam.simonini@gmail.com>
2021-07-30 11:45:04 -0400

    removed brace calls and adjusted farm.vue

--- src/components/Farm.vue
+++ src/components/Farm.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="farm">
-    <h3>
-      Welcome to {{yourName}}'s farm!
-      <img class="barn" src="/farm-images/barn.png">
-    </h3>
-    <div class="products">
-      <div class="crop-row">
-        <Carrots />
-        <Spinach />
-      </div>
-      <div class="livestock-row">
-        
-      </div>
-    </div>
-  </div>
+	<div class="farm">
+		<h3>
+			Welcome to {{ yourName }}'s farm!
+			<img class="barn" src="/farm-images/barn.png" />
+		</h3>
+		<div class="products">
+			<div class="crop-row">
+				<Carrots />
+				<!-- There seems to be a bug below! The spinach component produces a carrot image. Let's fix it! -->
+				<Spinach />
+			</div>
+			<div class="livestock-row"></div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -21,73 +20,73 @@ import Carrots from "./crops/Carrots.vue";
 import Spinach from "./crops/Spinach.vue";
 
 export default {
-  name: "Farm",
-  components: {
-    Carrots,
-    Spinach
-  },
-  data: function() {
-    return {
-      yourName: "{yourName}"
-    };
-  },
-  methods: {}
+	name: "Farm",
+	components: {
+		Carrots,
+		Spinach
+	},
+	data: function() {
+		return {
+			yourName: "{yourName}"
+		};
+	},
+	methods: {}
 };
 </script>
 
 <style scoped>
 .farm {
-  border-top: solid 1px#42b983;
-  margin-bottom: 20px;
+	border-top: solid 1px#42b983;
+	margin-bottom: 20px;
 }
 h3 {
-  font-size: 26px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	font-size: 26px;
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 .crop-row,
 .livestock-row,
 .numbers-row {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 100%;
-  padding: 15px 0;
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	width: 100%;
+	padding: 15px 0;
 }
 .crop:hover {
-  cursor: pointer;
+	cursor: pointer;
 }
 .crops > div {
-  margin: 0 10px;
+	margin: 0 10px;
 }
 .barn {
-  max-width: 50px;
+	max-width: 50px;
 }
 .products {
-  border: 1px solid red;
-  -webkit-box-shadow: 2px 1px 18px -6px rgba(235, 0, 86, 1);
-  -moz-box-shadow: 2px 1px 18px -6px rgba(235, 0, 86, 1);
-  box-shadow: 2px 1px 18px -6px rgba(235, 0, 86, 1);
-  min-height: 100px;
-  border-radius: 20px;
+	border: 1px solid red;
+	-webkit-box-shadow: 2px 1px 18px -6px rgba(235, 0, 86, 1);
+	-moz-box-shadow: 2px 1px 18px -6px rgba(235, 0, 86, 1);
+	box-shadow: 2px 1px 18px -6px rgba(235, 0, 86, 1);
+	min-height: 100px;
+	border-radius: 20px;
 }
 </style>
 
 <style>
 /* global styles for our farm */
 .crop img {
-  width: 100px;
+	width: 100px;
 }
 .livestock img {
-  width: 70px;
-  max-width: 110px;
+	width: 70px;
+	max-width: 110px;
 }
 img {
-  max-width: 100%;
+	max-width: 100%;
 }
 .genetidMod {
-  width: 150px !important;
+	width: 150px !important;
 }
 </style>
--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -1,80 +1,68 @@
 <template>
-  <div class="">
-      <h2>Binding Data & Events</h2>
-      <p>
-          We already saw data binding in action when we changed the name of our farm. There, we did interpolation by using double curly braces to implant a javascript
-          variable into our template area. As the variable changes, the content within the HTML will follow suit. It's just that simple! 
-      </p>
-      <Editor v-model="miniOne" lang="javascript" width="500" height="100" />
-      <p><span class="challenge">Mini Challenge 1: </span>in the above IDE, change the value of someVariable to 'chicken'. What will be displayed in the p tag?</p>
-      <h3>Data binding</h3>
-      <p>
-          We can use Vue directives to do many things, one of which is binding of an input, as follows:
-      </p>
-      <h4>Data binding of text box: <span>{{userInput}}</span></h4>
-      <h4>Data binding of text box (again): <span>{{userInput}}</span></h4>
-        <v-flex xs12 sm6 md3>
-            <v-text-field
-                v-model="userInput"
-                color="#2E7D32"
-            ></v-text-field>
-        </v-flex>
-      <h4>Two way binding of text box: {{checkbox}}</h4>
-      <v-checkbox v-model="checkbox" color="#2E7D32"> </v-checkbox>
-      <Editor v-model="miniTwo" lang="javascript" width="500" height="120" />
-      <p><span class="challenge">Mini Challenge 2: </span> in the above IDE, make it so that the p tag has "default" as its default text. What happens if the user modifies the input value?</p>
+	<div class="">
+		<h2>Binding Data & Events</h2>
+		<p>
+			We already saw data binding in action when we changed the name of our farm. There, we did interpolation by using double curly braces to implant a javascript variable into our template area. As the variable changes, the content within the HTML will follow suit. It's just that simple!
+		</p>
+		<Editor v-model="miniOne" lang="javascript" width="500" height="100" />
+		<p><span class="challenge">Mini Challenge 1: </span>in the above IDE, change the value of someVariable to 'chicken'. What will be displayed in the p tag?</p>
+		<h3>Data binding</h3>
+		<p>
+			We can use Vue directives to do many things, one of which is binding of an input, as follows:
+		</p>
+		<h4>
+			Data binding of text box: <span>{{ userInput }}</span>
+		</h4>
+		<h4>
+			Data binding of text box (again): <span>{{ userInput }}</span>
+		</h4>
+		<v-flex xs12 sm6 md3>
+			<v-text-field v-model="userInput" color="#2E7D32"></v-text-field>
+		</v-flex>
+		<h4>Two way binding of text box: {{ checkbox }}</h4>
+		<v-checkbox v-model="checkbox" color="#2E7D32"> </v-checkbox>
+		<Editor v-model="miniTwo" lang="javascript" width="500" height="120" />
+		<p><span class="challenge">Mini Challenge 2: </span> in the above IDE, make it so that the p tag has "default" as its default text. What happens if the user modifies the input value?</p>
 
-      <h3>Directives and Events</h3>
-      <p>
-          Vue directives are syntax we add to the template area, which enables that template area to become dynamic in some way. In other words, you can expect 
-          "javascripty stuff" to be happening when using "v-"
-      </p>
-      <p>
-          We can use Vue directives to add event listeners. For that, we'll use v-on: + event listener's name = function to be triggered by the event. For example, "v-on:click=harvest" 
-          (or "@click=harvest" for the short form). That code made the click trigger "harvest". Now we need to define harvest. We can do that in the script area of our file, within the export object. 
-          Add a "methods" property to the export object, and define your function in there. Check the code on this page and make sure you understand what is going on with the click event. It's important for the next challenge.
-      </p>
-      <Editor v-model="miniThree" lang="javascript" width="500" height="120" />
-      <p><span class="challenge">Mini Challenge 3: </span> in the above IDE, make the second button also transmit an alert on click.</p>
+		<h3>Directives and Events</h3>
+		<p>
+			Vue directives are syntax we add to the template area, which enables that template area to become dynamic in some way. In other words, you can expect "javascripty stuff" to be happening when using "v-"
+		</p>
+		<p>
+			We can use Vue directives to add event listeners. For that, we'll use v-on: + event listener's name = function to be triggered by the event. For example, "v-on:click=harvest" (or "@click=harvest" for the short form). That code made the click trigger "harvest". Now we need to define harvest. We can do that in the script area of our file, within the export object. Add a "methods" property to the export object, and define your function in there. Check the code on this page and make sure you understand what is going on with the click event. It's important for the next challenge.
+		</p>
+		<Editor v-model="miniThree" lang="javascript" width="500" height="120" />
+		<p><span class="challenge">Mini Challenge 3: </span> in the above IDE, make the second button also transmit an alert on click.</p>
 
-      <v-btn v-on:click='harvest'>Power On Harvester</v-btn>
-  </div>
+		<v-btn v-on:click="harvest">Power On Harvester</v-btn>
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'DataBinding',
-  components: {
-    Editor: require('vue2-ace-editor'),
-  },
-  data: function () {
-    return {
-        miniOne: "<p>{{ someVariable }}</p> \n\n data: function() {\n  return { \n   someValiable: 'test'\n  } \n}",
-        miniTwo: "<input type='text' v-model='userInput' /> \n<p>{{ userInput }}</p> \n\n data: function() {\n  return { \n   userInput: ''\n  } \n}",
-        miniThree: "<button @click='sendAlert'></button> \n<button></button>  \n\nmethods: { \n sendAlert: function() {\n   alert('you clicked me') \n } \n}",
-        userInput: '',
-        checkbox: false
-    }
-  },
-  methods: {
-    editorInit: function () {
-      require('brace/ext/language_tools') //language extension prerequsite...
-      require('brace/mode/html')                
-      require('brace/mode/javascript')    //language
-      require('brace/mode/less')
-      require('brace/theme/chrome')
-      require('brace/snippets/javascript') //snippet
-    },
-    harvest: function() {
-        alert('Alert: harvester powered on!')
-    }
-  }
-}
+	name: "DataBinding",
+	components: {
+		Editor: require("vue2-ace-editor")
+	},
+	data: function() {
+		return {
+			miniOne: "<p>{{ someVariable }}</p> \n\n data: function() {\n  return { \n   someValiable: 'test'\n  } \n}",
+			miniTwo: "<input type='text' v-model='userInput' /> \n<p>{{ userInput }}</p> \n\n data: function() {\n  return { \n   userInput: ''\n  } \n}",
+			miniThree: "<button @click='sendAlert'></button> \n<button></button>  \n\nmethods: { \n sendAlert: function() {\n   alert('you clicked me') \n } \n}",
+			userInput: "",
+			checkbox: false
+		};
+	},
+	methods: {
+		harvest: function() {
+			alert("Alert: harvester powered on!");
+		}
+	}
+};
 </script>
 
 <style scoped>
-    h4 span {
-        color: #2E7D32;
-    }
+h4 span {
+	color: #2e7d32;
+}
 </style>
